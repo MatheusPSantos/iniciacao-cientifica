8773d917bbf8d944b78207a1814219584baaaa69
matheuspsantos <matheuspereira8187@gmail.com>
2018-04-05 14:46:40 -0300

    mudança feita para o banco ic-db

--- validacao.php
+++ validacao.php
@@ -8,10 +8,11 @@ $login = $_POST['numero_siape'];
 $senha =$_POST['senha'];
 
 // conexão com o banco de dados
-$con = mysqli_connect("127.0.0.1", "root", "jxtnlx33", "server");
+$con = mysqli_connect("127.0.0.1", "root", "jxtnlx33", "ic-db");
 
 // a variável $result pega o valor de login e senha e faz uma busca na tabela de usuarios
-$result = mysqli_query($con, "SELECT * FROM `usuario` WHERE `NOME` = '$login' AND `SENHA`='$senha'");
+$result = mysqli_query($con, "SELECT * FROM `professor` WHERE `prof_siape` = '$login' AND `prof_senha`='$senha'");
+$queryAdmin = mysqli_query($con, "SELECT * FROM `admin` WHERE `admin_nome` ='$login' AND `admin_senha` = '$senha'");
 
 // bloco com if e else, verificando se $result foi bem sucessida
 // se retorna 1 a busca foi bem sucedida, se retornar 0 houve algum erro na busca
@@ -20,7 +21,9 @@ if(mysqli_num_rows($result) > 0) {
     $_SESSION['senha'] = $senha;
     header('location:paginas/horarios.php');
 } 
-else if($login == "admin" && $senha == "admin") {
+else if(mysqli_num_rows($queryAdmin) > 0) {
+    $_SESSION['numero_siape'] = $login;
+    $_SESSION['senha'] = $senha;
     header('location:paginas/admin.php');
 }
 else {
